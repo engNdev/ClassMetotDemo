ab593adfd8ecf96c18b4008cdfa89b6058610790
Engin Celal Taşkın <engin48ect@gmail.com>
2021-01-18 01:09:29 +0300

    v2

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi. \nMüşteri Adı: " + musteri.musteriAd + "\nMüşteri Soyad: " + musteri.musteriSoyad);
+        }
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("-----------------------");
+                Console.WriteLine("Müşteri Id: " + musteri.musteriId +
+                    "\nMüşteri Ad: " + musteri.musteriAd +
+                    "\nMüşteri Soyad: " + musteri.musteriSoyad);
+                Console.WriteLine("-----------------------");
+            }
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("\n" + musteri.musteriId +" ID numaralı " 
+                + musteri.musteriAd +" " + musteri.musteriSoyad 
+                + " Ad soyadlı müşterimiz silinmiştir.\n");
+        }
+    }
+    
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,38 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+            Musteri musteri3 = new Musteri();
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteri1.musteriAd = "Engin";
+            musteri1.musteriSoyad = "Taşkın";
+            musteri1.musteriId = 1;
+
+            musteri2.musteriAd = "Ahmet";
+            musteri2.musteriSoyad = "Öz";
+            musteri2.musteriId = 2;
+
+            musteri3.musteriAd = "Mehmet";
+            musteri3.musteriSoyad = "Taş";
+            musteri3.musteriId = 3;
+
+            Musteri[] musteriler = { musteri1, musteri2, musteri3 };
+
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriSilme(musteri1);
+
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriSilme(musteri2);
+
+            musteriManager.MusteriEkle(musteri3);
+            musteriManager.MusteriSilme(musteri3);
+
+            musteriManager.MusteriListele(musteriler);
+
         }
     }
+    
+  
 }
